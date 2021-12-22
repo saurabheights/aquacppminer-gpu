0a31cd60ba14ad637d849d0fcf5e60fd83fb0e3e
Saurabh Khanduja <saurabh.khanduja@ikara.ai>
2021-12-22 05:44:28 +0100

    No need of pool addresses in configuration.

--- src/miningConfig.cpp
+++ src/miningConfig.cpp
@@ -10,16 +10,6 @@
 
 static MiningConfig s_cfg;
 
-// const std::string FEES_ADDRESS = "0x59cf04d83051dd52b5b96bf9c5742684e93bd800";
-const std::vector<std::string> POOLS = {
-    "http://aqua.signal2noi.se:19998/",
-    "http://pool.aquachain-foundation.org:8888/",
-    "http://nl.aquachain-foundation.org:8888/",
-    "http://aquacha.in:8888/",
-    "http://aquapool.rplant.xyz:19998/",
-    "http://aqua.cnpool.vip:8888/",
-    "http://aqua.dapool.me:3333/"};
-
 void initMiningConfig() {
     s_cfg.defaultSubmitWorkUrl = "http://127.0.0.1:8543";
     s_cfg.getWorkUrl = s_cfg.defaultSubmitWorkUrl;
